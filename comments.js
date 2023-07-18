6c699cb362688fea51828e6e296304f0917e556c
Aielumoh Peace <77479372+Peace-Aielumoh@users.noreply.github.com>
2023-07-18 14:38:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,36 @@
 //Create web Server
+
 const express = require('express');
-const router = express.Router();
-const Comment = require('../models/Comment');
+const app = express();
+const port = 3000;
 
-//Get All Comments
-router.get('/', async (req, res) => {
-    try {
-        const comments = await Comment.find();
-        res.json(comments);
-    } catch (err) {
-        res.json({ message: err });
-    }
-});
+//import the routes
+const routes = require('./routes');
+const cors = require('cors');
 
-//Submit a Comment
-router.post('/', async (req, res) => {
-    const comment = new Comment({
-        comment: req.body.comment,g
+//import the db connection
+const db = require('./config/db');
+
+//import the model
+const Comment = require('./models/Comment');
+
+//import the body parser
+const bodyParser = require('body-parser');
+
+//import the cors
+app.use(cors());
+
+//use the body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//connect to the db
+db.authenticate()
+    .then(() => console.log('Database connected successfully'))
+    .catch(err => console.log('Error: ' + err))
+
+//define the routes
+app.use('/', routes);
+
+//start the server
+app.listen(port, () => console.log(`Server running on port ${port}`));
